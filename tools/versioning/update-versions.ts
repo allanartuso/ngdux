e6a74c0279bdee9c960f882d9670c56366d0a545
Allan Artuso <allan.artuso@gmail.com>
2023-05-16 19:08:35 +0200

    chore: improve update versions

--- tools/versioning/update-versions.ts
+++ tools/versioning/update-versions.ts
@@ -1,9 +1,10 @@
-import { execSync } from 'node:child_process';
+import * as fs from 'fs';
 import { ProjectGraphProjectNode } from 'nx/src/config/project-graph';
 import { filterAffected } from 'nx/src/project-graph/affected/affected-project-graph';
 import { calculateFileChanges } from 'nx/src/project-graph/file-utils';
 import { createProjectGraphAsync } from 'nx/src/project-graph/project-graph';
 import { parseFiles } from 'nx/src/utils/command-line-utils';
+import standardVersion from 'standard-version';
 
 interface PackageJson {
   name: string;
@@ -24,6 +25,8 @@ interface ProjectJson {
 }
 
 interface ProjectInfo {
+  projectJsonFile: string;
+  packageJsonFile: string;
   packageJson: PackageJson;
   projectJson: ProjectJson;
 }
@@ -35,30 +38,28 @@ async function updatePublishableLibsVersions() {
   const files = parseFiles({ base: BASE_BRANCH }).files;
   const changes = calculateFileChanges(files, []);
   const affected = await filterAffected(graph, changes);
-  const publishableProjects = await getPublishableLibs(Object.values(affected.nodes));
-  const latestVersion = getLatestVersion(publishableProjects);
-  const cmd = `git log --pretty=%s master...v${latestVersion}`;
 
-  const commitMessages = execSync(cmd).toString().split('\n');
+  await standardVersion({
+    skip: {
+      commit: true,
+      tag: true
+    }
+  });
 
-  const structuralElements = commitMessages.map(commitMessage => commitMessage.split(':')[0]);
-  console.log(structuralElements);
+  const nextVersion = await getNextVersion();
+  const publishableLibs = await getPublishableLibs(Object.values(affected.nodes));
+  await updatePubLibsVersion(publishableLibs, nextVersion);
 
-  const isMajor = structuralElements.some(el => el.includes('!') || el.includes('BREAKING CHANGE'));
-  if (isMajor) {
-    console.log('MAJOR');
-    process.exit(0);
-  }
-  const isMinor = structuralElements.some(el => el.includes('feat'));
-  if (isMinor) {
-    console.log('MINOR');
-    process.exit(0);
-  }
-  const isPatch = structuralElements.length;
-  if (isPatch) {
-    console.log('PATCH');
-    process.exit(0);
-  }
+  // build
+
+  await standardVersion({
+    skip: {
+      bump: true,
+      changelog: true
+    }
+  });
+
+  // publish
 
   process.exit(0);
 }
@@ -74,30 +75,40 @@ async function getPublishableLibs(list: ProjectGraphProjectNode[]) {
   return projectFiles.filter(file => !!file.packageJson);
 }
 
-async function getProjectInfo(node: ProjectGraphProjectNode) {
-  const projectJsonFile = '../../' + node.data.root + '/project.json';
-  const packageJsonFile = '../../' + node.data.root + '/package.json';
+async function getProjectInfo(node: ProjectGraphProjectNode): Promise<ProjectInfo> {
+  const packageJsonFile = node.data.root + '/package.json';
+  const projectJsonFile = node.data.root + '/project.json';
 
-  let projectJson;
   let packageJson;
+  let projectJson;
 
   try {
-    packageJson = await import(packageJsonFile);
-    projectJson = await import(projectJsonFile);
+    packageJson = await import(`'../../${packageJsonFile}`);
+    projectJson = await import(`'../../${projectJsonFile}`);
   } catch (e) {}
 
   return {
+    projectJsonFile,
+    packageJsonFile,
     projectJson,
     packageJson
   };
 }
 
-function getLatestVersion(projects: ProjectInfo[]) {
-  const versions = projects.map(p => p.packageJson.version).sort();
-  const latestVersion = versions.pop();
-  return latestVersion;
+async function getNextVersion(): Promise<string> {
+  const packageJsonFile = '../../package.json';
+  const packageJson = await import(packageJsonFile);
+
+  return packageJson.version;
 }
 
-function getUpdateType() {}
+async function updatePubLibsVersion(publishableLibs: ProjectInfo[], nextVersion: string) {
+  for (const lib of publishableLibs) {
+    const newPackageJson = { ...lib.packageJson, version: nextVersion };
+
+    await fs.promises.writeFile(lib.packageJsonFile, JSON.stringify(newPackageJson));
+    console.log(`${lib.projectJson.name} version updated to ${nextVersion}`);
+  }
+}
 
 updatePublishableLibsVersions();
