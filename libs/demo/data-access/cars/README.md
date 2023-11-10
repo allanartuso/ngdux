916273fe0d4ff2cf47c34f280edca82458c9b625
Allan Artuso <allan.artuso@gmail.com>
2023-11-10 11:28:24 +0100

    feat: add cars data-access

---
+++ libs/demo/data-access/cars/README.md
@@ -0,0 +1,7 @@
+# demo-data-access-cars
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test demo-data-access-cars` to execute the unit tests.
---
+++ libs/demo/data-access/cars/src/lib/services/cars.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { UserDto, USERS_RESOURCE_BASE_PATH } from '@demo/demo/data-model/users';
+import { RestService } from '@demo/shared/data-access';
+import { ErrorDto, ListService, RequestOptions } from '@ngdux/data-model-common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CarsService implements ListService<UserDto> {
+  constructor(private readonly restService: RestService) {}
+
+  loadResources(options: RequestOptions): Observable<UserDto[]> {
+    return this.restService.loadResources(USERS_RESOURCE_BASE_PATH, options);
+  }
+
+  deleteResources(ids: string[]): Observable<Array<ErrorDto>> {
+    return this.restService.deleteResources(USERS_RESOURCE_BASE_PATH, ids);
+  }
+
+  patchResources(ids: string[], resource: Partial<UserDto>): Observable<Array<UserDto | ErrorDto>> {
+    return this.restService.patchResources(
+      USERS_RESOURCE_BASE_PATH,
+      ids.map(id => ({ id, resource }))
+    );
+  }
+}
